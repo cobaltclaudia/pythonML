7b7745f05a27ded50f70606f3d520b23fa7a75cd
claudia <ceruleancee@gmail.com>
2023-03-22 20:39:37 -0500

    update desiredTime.py

--- desiredTime.py
+++ desiredTime.py
@@ -37,5 +37,32 @@ print(df['returns_final'].size)
 print(df['returns_final'])
 
 # apply computeClassification and show 1/-1 in adjacent column
-df.iloc[:, len(df.columns) - 1] = df.iloc[:, len(df.columns) - 1].apply(computeClassification)
+df[df.columns[len(df.columns) - 1]] = df[df.columns[len(df.columns) - 1]].apply(computeClassification)
 print(df.iloc[:, len(df.columns) - 1])
+
+# convert float to int
+# 2nd half is forward tested on
+testData = df[-int((len(df) * 0.10)):]
+
+# 1st half is trained on
+trainData = df[-int((len(df) * 0.90)):]
+
+# replace all inf with nan
+testData_1 = testData.replace([np.inf, -np.inf], np.nan)
+trainData_1 = trainData.replace([np.inf, -np.inf], np.nan)
+
+# replace all nans with 0
+testData_2 = testData_1.fillna(0)
+trainData_2 = trainData_1.fillna(0)
+
+# x is the list of features
+data_x_train = trainData_2.iloc[:, 0:len(trainData_2.columns) - 1]
+
+# y is the 1 or -1 value to be predicted
+data_y_train = trainData_2.iloc[:, 0:len(trainData_2.columns) - 1]
+
+# same thing for the test dataset
+data_x_test = testData_2.iloc[:, 0:len(testData_2.columns) - 1]
+data_y_test = testData_2.iloc[:, 0:len(testData_2.columns) - 1]
+
+
